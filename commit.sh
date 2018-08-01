48f577c37aa5f00f07340bb833bbb509ed0a74ad
soju6jan <cybersol@naver.com>
2018-08-02 03:15:36 +0900

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ export HOME=/var/services/homes/soju6jan
 NowDate=$(date +%Y%m%d)-$(date +%H%M) 
 echo $NowDate
 cd /volume1/homes/soju6jan/git/soju6jan.github.io
-git add avnori_ymav.xml
+git add avnori_ymav.xml 
 git add avnori_nmav.xml
 git add klive.xml
 git add xmltv.xml
