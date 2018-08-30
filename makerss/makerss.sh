cbdb85a18561879e6095b4ac5f3c4f3396695e5b
soju6jan <cybersol@naver.com>
2018-08-30 20:25:56 +0900

    20180830-2025

--- makerss/makerss.sh
+++ makerss/makerss.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 PATH_MAKERSS=/volume1/web/makerss
 PATH_GIT=/volume1/soju6jan/homes/soju6jan/git/soju6jan.github.io
-cd $PATH_GIT
+
+cd $PATH_MAKERSS
 python makerss_main.py
-mv *.xml $PATH_MAKERSS/rss/
+mv $PATH_MAKERSS/*.xml $PATH_GIT
 $PATH_GIT/commit.sh
 
