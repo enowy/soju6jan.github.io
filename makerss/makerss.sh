7d2440f4c62304c064c56d6915c451b0e47f762e
soju6jan <cybersol@naver.com>
2018-08-30 14:37:02 +0900

    20180830-1437

---
+++ makerss/makerss.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /volume1/web/makerss
+python makerss_main.py
+mv *.xml /volume1/soju6jan/homes/soju6jan/git/soju6jan.github.io/rss/
+/volume1/homes/soju6jan/git/soju6jan.github.io/commit.sh
+
---
+++ makerss/makerss_main.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+import sys, os, time
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+from makerss_setting import *
+
+def MakeRssFeed(where, cate, list):
+	str =  '<rss xmlns:showrss=\"http://showrss.info/\" version=\"2.0\">\n'
+	str += '\t<channel>\n'
+	str += '\t\t<title>' + '�䷻Ʈ'.decode('euc-kr') + ' %s - %s</title>\n' % (where, cate)
+	str += '\t\t<link></link>\n'
+	str += '\t\t<description></description>\n'
+	for item in list:
+		str += '\t\t<item>\n'
+		str += '\t\t\t<title>%s</title>\n' % item['title']
+		str += '\t\t\t<link>%s</link>\n' % item['link']
+		#str += '\t\t\t<description>%s</description>\n' % item['title']
+		str += '\t\t\t<showrss:showid></showrss:showid>\n'
+		str += '\t\t\t<showrss:showname>%s</showrss:showname>\n' % item['title']
+		str += '\t\t</item>\n'
+	str += '\t</channel>\n'
+	str += '</rss>'
+	return str
+
+def WriteFile(filename, data ):
+	try:
+		#with open(filename, "w", encoding='utf8') as f:
+		with open(filename, "w") as f:
+			f.write( unicode(data) )
+		f.close()
+		return
+	except Exception as e:
+		print('W11:%s' % e)
+		pass
+	try:
+		with open(filename, "w", encoding='utf8') as f:
+		#with open(filename, "w") as f:
+			f.write( data )
+		f.close()
+		return
+	except Exception as e:
+		print('W22:%s' % e)
+		pass
+ 
+def download(driver, download_url, filename):
+	print('Injecting retrieval code into web page')
+	driver.execute_script("""
+	    window.file_contents = null;
+	    var xhr = new XMLHttpRequest();
+	    xhr.responseType = 'blob';
+	    xhr.onload = function() {
+	        var reader  = new FileReader();
+	        reader.onloadend = function() {
+	            window.file_contents = reader.result;
+	        };
+	        reader.readAsDataURL(xhr.response);
+	    };
+	    xhr.open('GET', %(download_url)s);
+	    xhr.send();
+	""".replace('\r\n', ' ').replace('\r', ' ').replace('\n', ' ') % {
+	    'download_url': json.dumps(download_url),
+	})
+
+	print('Looping until file is retrieved')
+	downloaded_file = None
+	while downloaded_file is None:
+	    # Returns the file retrieved base64 encoded (perfect for downloading binary)
+	    downloaded_file = driver.execute_script('return (window.file_contents !== null ? window.file_contents.split(\',\')[1] : null);')
+	    #print(downloaded_file)
+	    if not downloaded_file:
+	        print('\tNot downloaded, waiting...')
+	        time.sleep(0.5)
+	print('\tDone')
+
+	print('Writing file to disk')
+	fp = open(filename, 'wb')
+	fp.write(base64.b64decode(downloaded_file))
+	fp.close()
+	print('\tDone')
+
+
+def GetDriver():
+	driver = webdriver.Remote(command_executor='http://127.0.0.1:8910', desired_capabilities=DesiredCapabilities.PHANTOMJS)
+	driver.implicitly_wait(10)
+	return driver
+
+def Start(site):
+	print('MAKERSS START : %s' % site['TORRENT_SITE_TITLE'])
+	for cate in site['BO_TABLE_LIST']:
+		print('CATE : %s' % cate)
+		list = GetList(driver, site, cate)
+		if len(list) == 0: return -1
+		str = MakeRssFeed(site['TORRENT_SITE_TITLE'], cate, list)
+		#print('RSS : %s' % str)
+		WriteFile('%s_%s.xml' % (site['TORRENT_SITE_TITLE'], cate), str)
+		time.sleep(10)
+
+
+if __name__ == "__main__":
+	global driver
+	driver = GetDriver()
+	for site in SITE_LIST:
+		Start(site)
+	driver.quit()
