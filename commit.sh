1895f893cec77f46a1649e8d89459ace85a97542
soju6jan <soju6jan@gmail.com>
2018-08-30 19:41:19 +0900

    test

--- commit.sh
+++ commit.sh
@@ -2,11 +2,7 @@
 export LANG=en_US.utf8
 export HOME=/var/services/homes/soju6jan
 NowDate=$(date +%Y%m%d)-$(date +%H%M) 
-echo $NowDate
-cd /volume1/homes/soju6jan/git/soju6jan.github.io
-git add avnori_ymav.xml 
-git add avnori_nmav.xml
-git add klive.xml
-git add xmltv.xml
+cd /volume1/video/git/soju6jan.github.io
+git add rss/torrentboza_ani.xml
 git commit -m $NowDate
 git push
